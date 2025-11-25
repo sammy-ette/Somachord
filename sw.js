983c8b01c108ef2cb9da010542a03e57d0ca1ed6
sammy-ette <torchedsammy@gmail.com>
2025-11-25 11:02:37 -0400

    refactor: use workbox for service worker

--- sw.js
+++ sw.js
@@ -1,99 +1,24 @@
-const putInCache = async (request, response) => {
-	const cache = await caches.open("v1")
-	await cache.put(request, response)
-}
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+);
 
-async function networkThenCache(event) {
-	try {
-		const responseFromNetwork = await fetch(event.request)
-		event.waitUntil(putInCache(event.request, responseFromNetwork.clone()))
-		return responseFromNetwork
-	} catch(e) {
-		const responseFromCache = await caches.match(event.request)
-		if (responseFromCache) {
-			return responseFromCache
-		}
+workbox.routing.setDefaultHandler(new workbox.strategies.StaleWhileRevalidate())
 
-		return new Response("Offline...", {
-			status: 408,
-			headers: { "Content-Type": "text/plain" },
-		})
-	}
-}
+workbox.routing.registerRoute(({url}) => url.pathname.includes('somachord'), new workbox.strategies.NetworkFirst())
 
-async function cacheFirst(event) {
-	const responseFromCache = await caches.match(event.request)
-	if (responseFromCache) {
-		return responseFromCache
-	}
+workbox.routing.registerRoute(({url}) => url.pathname.includes('getPlaylist.view')
+  || url.pathname.includes('getStarred2.view')
+  || url.pathname.includes('getSimilarSongs.view')
+  || url.pathname.includes('getSimilarSongs2.view'), new workbox.strategies.NetworkFirst())
 
-	try {
-		const responseFromNetwork = await fetch(event.request)
-		event.waitUntil(putInCache(event.request, responseFromNetwork.clone()))
-		return responseFromNetwork
-	} catch (error) {
-		return new Response("Offline...", {
-			status: 408,
-			headers: { "Content-Type": "text/plain" },
-		})
-	}
-}
+workbox.routing.registerRoute(({url}) => url.pathname.includes('search3.view')
+  || url.pathname.includes('scrobble.view')
+  || url.pathname.includes('star.view')
+  || url.pathname.includes('unstar.view')
+  || url.pathname.includes('getPlayQueue.view')
+  || url.pathname.includes('updatePlaylist.view')
+  || url.pathname.includes('createPlaylist.view')
+  || url.pathname.includes('savePlayQueue.view'), new workbox.strategies.NetworkOnly())
 
-self.addEventListener("fetch", (event) => {
-	const url = new URL(event.request.url)
-	if (url.protocol === 'chrome-extension:') {
-		return
-	}
-
-	const cacheFirstList = ["/rest/getCoverArt.view"]
-	const isCacheFirst = cacheFirstList.some(path => event.request.url.includes(path))
-
-	const networkOnly = ["/rest/scrobble.view", "/rest/stream.view"]
-	const isNetworkOnly = networkOnly.some(path => event.request.url.includes(path))
-
-	if(isNetworkOnly) {
-		return
-	}
-
-	// Source: https://samdutton.github.io/samples/service-worker/prefetch-video/
-	if (event.request.headers.get('range')) {
-		let pos = Number(/^bytes\=(\d+)\-$/g.exec(event.request.headers.get('range'))[1]);
-		console.log('Range request for', event.request.url, ', starting position:', pos);
-
-		let response = caches.open("music")
-		.then((cache) => {
-			return cache.match(event.request.url);
-		})
-		.then((res) => {
-			if (!res) {
-				return fetch(event.request).then(res => {
-					return res.arrayBuffer()
-				})
-			}
-			return res.arrayBuffer()
-		})
-		.then((ab)=> {
-			return new Response(ab.slice(pos), {
-				status: 206,
-				statusText: 'Partial Content',
-				headers: [
-				// ['Content-Type', 'video/webm'],
-				['Content-Range', 'bytes ' + pos + '-' + (ab.byteLength - 1) + '/' + ab.byteLength]]
-			})
-		})
-
-		event.respondWith(response)
-		return;
-	}
-
-	if(isCacheFirst) {
-		event.respondWith(
-			cacheFirst({
-				request: event.request,
-				event,
-			}),
-		)
-	} else {
-		event.respondWith(networkThenCache(event))
-	}
-})
+workbox.routing.registerRoute(({url}) => url.pathname.includes('getCoverArt.view'), new workbox.strategies.CacheFirst())
+workbox.routing.registerRoute(({url}) => url.pathname.includes('stream.view'), new workbox.strategies.CacheFirst({plugins: [new workbox.rangeRequests.RangeRequestsPlugin()]}))
